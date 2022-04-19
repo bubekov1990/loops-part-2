0b3296e193ad1133f3105217a7a82f396d54bca1
bubekov1990 <bobbob1990@mail.ru>
2022-04-19 23:59:59 +0300

    New commit

--- src/main/java/ru/netology/StatsService.java
+++ src/main/java/ru/netology/StatsService.java
@@ -4,8 +4,8 @@ public class StatsService {
 
     public int calculateSum(int[] sales) {
         int sum = 0;
-        for (int sale : sales) {
-            sum = sum + sale;
+        for (int i : sales) {
+            sum += i;
         }
         return sum;
     }
@@ -14,28 +14,10 @@ public class StatsService {
         return calculateSum(sales) / sales.length;
     }
 
-    public int minSales(int[] sales) {
-        int minMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
-        for (long sale : sales) {
-            // sales[minMonth] - продажи в месяце minMonth
-            // sale - продажи в рассматриваемом месяце
-            if (sale <= sales[minMonth]) {
-                minMonth = month;
-            }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
-        }
-        return minMonth + 1;
-    }
-
-    public int aboveTheAverage(int[] sales) {
-        return calculateSum(sales) / sales.length;
-    }
-
     public int maxSales(int[] sales) {
         int minMonth = 0;
         int month = 0; // переменная для индекса рассматриваемого месяца в массиве
-        for (long sale : sales) {
+        for (int sale : sales) {
             // sales[minMonth] - продажи в месяце minMonth
             // sale - продажи в рассматриваемом месяце
             if (sale >= sales[minMonth]) {
@@ -46,22 +28,37 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int belowAvg(int[] sales) {
-        int below = 0;
-        for (long sale : sales) {
-            if (sale < 15) {
-                below++;
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (int sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
             }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
         }
-        return below;
+        return minMonth + 1;
     }
-    public int aboveAvg(int[] sales) {
-        int below = 0;
-        for (long sale : sales) {
-            if (sale > 15) {
-                below++;
+
+    public int belowTheAverage(int[] sales) {
+        int number = 0;
+        for (int sale : sales) {
+            if (sale < calcAvg(sales)) {
+                number = number + 1;
             }
         }
-        return below;
+        return number;
+    }
+
+    public int aboveTheAverage(int[] sales) {
+        int number = 0;
+        for (int sale : sales) {
+            if (sale > calcAvg(sales)) {
+                number = number + 1;
+            }
+        }
+        return number;
     }
 }
--- src/test/java/ru/netology/StatsServiceTest.java
+++ src/test/java/ru/netology/StatsServiceTest.java
@@ -10,67 +10,61 @@ class StatsServiceTest {
     @Test
     void shouldCalculateSum() {
         StatsService service = new StatsService();
+
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
-
         int actual = service.calculateSum(sales);
-
         assertEquals(expected, actual);
+
     }
 
     @Test
-    void shouldCalculateAvg() {
+    void shouldAverageSalesAmount() {
         StatsService service = new StatsService();
+
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
-
         int actual = service.calcAvg(sales);
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldCalculateMin() {
+    void shouldPeakSales() {
         StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.maxSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMinSales() {
+        StatsService service = new StatsService();
+
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 9;
-
         int actual = service.minSales(sales);
-
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    void shouldCalculateMax() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 7;
-
-        int actual = service.maxSales(sales);
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldBelowAvg() {
+    void shouldBelowTheAverage() {
         StatsService service = new StatsService();
+
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
-
-        int actual = service.belowAvg(sales);
-
+        int actual = service.belowTheAverage(sales);
         assertEquals(expected, actual);
     }
 
     @Test
-    void shouldAboveAvg() {
+    void shouldAboveTheAverage() {
         StatsService service = new StatsService();
+
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
-
-        int actual = service.aboveAvg(sales);
-
+        int actual = service.aboveTheAverage(sales);
         assertEquals(expected, actual);
     }
 
